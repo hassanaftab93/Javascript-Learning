924d4cbe34d702e717a1e9a19f206402808b130f
Hassan Aftab <hassanaftab.pk@gmail.com>
2023-01-20 16:04:15 +0500

    Functions

--- src/index.js
+++ src/index.js
@@ -9,10 +9,41 @@ const interestRate = 1.5;
 
 //Primitives:
 
-let name = "Hassan"; //String
-let age = 5; //Number
+let name = "Hassan";    //String
+let age = 5;            //Number
 let isApproved = false; //Boolean
-let value = undefined; //Undefined
-let color = null; //null
+let value = undefined;  //Undefined | Type + Value
+let color = null;       //null      | Object
 
 //ReferenceTypes:
+
+let person = {
+    name: 'Hassan',
+    age: 30
+};
+
+person.name = 'Overwritten Name'
+
+console.log(person);
+
+console.clear();
+
+let selectedColors = ['red','blue','green'];
+selectedColors[selectedColors.length] = 'purple';
+selectedColors[selectedColors.length] = 'silver';
+console.log(selectedColors);
+
+console.clear();
+
+//Functions
+
+function greet(name) {
+    console.log('Hello '+ name);
+}
+
+function square(number) {
+    return number*number;
+}
+
+greet(firstName);
+console.log('Squared value: '+square(2));
