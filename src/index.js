f7169b88fe41af83ba8e3c519cc9b7e190310586
Hassan Aftab <hassanaftab.pk@gmail.com>
2023-01-20 15:27:00 +0500

    Primitives and Reference Types

--- src/index.js
+++ src/index.js
@@ -1,2 +1,18 @@
 // This is a comment
+
+let firstName = "Hassan";
+let lastName = "Aftab";
+console.log(firstName,lastName);
 console.log("Hello World with JS!");
+
+const interestRate = 1.5;
+
+//Primitives:
+
+let name = "Hassan"; //String
+let age = 5; //Number
+let isApproved = false; //Boolean
+let value = undefined; //Undefined
+let color = null; //null
+
+//ReferenceTypes:
