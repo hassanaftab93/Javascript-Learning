014a43cfd183ee3b6a69ff127b507f6e0d6f0129
Hassan Aftab <hassanaftab.pk@gmail.com>
2023-01-20 16:55:51 +0500

    If Else

--- src/index.js
+++ src/index.js
@@ -1,49 +1,58 @@
-// This is a comment
+// // This is a comment
 
-let firstName = "Hassan";
-let lastName = "Aftab";
-console.log(firstName,lastName);
-console.log("Hello World with JS!");
+// let firstName = "Hassan";
+// let lastName = "Aftab";
+// console.log(firstName,lastName);
+// console.log("Hello World with JS!");
 
-const interestRate = 1.5;
+// const interestRate = 1.5;
 
-//Primitives:
+// //Primitives:
 
-let name = "Hassan";    //String
-let age = 5;            //Number
-let isApproved = false; //Boolean
-let value = undefined;  //Undefined | Type + Value
-let color = null;       //null      | Object
+// let name = "Hassan";    //String
+// let age = 5;            //Number
+// let isApproved = false; //Boolean
+// let value = undefined;  //Undefined | Type + Value
+// let color = null;       //null      | Object
 
-//ReferenceTypes:
+// //ReferenceTypes:
 
-let person = {
-    name: 'Hassan',
-    age: 30
-};
+// let person = {
+//     name: 'Hassan',
+//     age: 30
+// };
 
-person.name = 'Overwritten Name'
+// person.name = 'Overwritten Name'
 
-console.log(person);
+// console.log(person);
 
-console.clear();
+// console.clear();
 
-let selectedColors = ['red','blue','green'];
-selectedColors[selectedColors.length] = 'purple';
-selectedColors[selectedColors.length] = 'silver';
-console.log(selectedColors);
+// let selectedColors = ['red','blue','green'];
+// selectedColors[selectedColors.length] = 'purple';
+// selectedColors[selectedColors.length] = 'silver';
+// console.log(selectedColors);
 
-console.clear();
+// console.clear();
 
-//Functions
+// //Functions
 
-function greet(name) {
-    console.log('Hello '+ name);
-}
+// function greet(name) {
+//     console.log('Hello '+ name);
+// }
 
-function square(number) {
-    return number*number;
-}
+// function square(number) {
+//     return number*number;
+// }
 
-greet(firstName);
-console.log('Squared value: '+square(2));
+// greet(firstName);
+// console.log('Squared value: '+square(2));
+
+let hour = 20;
+
+if(hour >= 6 && hour <12)
+    console.log('Good Morning');
+else if (hour >=12 && hour < 18)
+    console.log('Good Afternoon');
+else
+    console.log('Good Evening');
