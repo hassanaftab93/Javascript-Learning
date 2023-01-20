e14158242c0667f476eac34763e24f3de755fb07
Hassan Aftab <hassanaftab.pk@gmail.com>
2023-01-20 17:03:38 +0500

    Control Flow 4.2 Switch Case

--- src/index.js
+++ src/index.js
@@ -48,11 +48,29 @@
 // greet(firstName);
 // console.log('Squared value: '+square(2));
 
-let hour = 20;
+// let hour = 20;
 
-if(hour >= 6 && hour <12)
-    console.log('Good Morning');
-else if (hour >=12 && hour < 18)
-    console.log('Good Afternoon');
-else
-    console.log('Good Evening');
+// if(hour >= 6 && hour <12)
+//     console.log('Good Morning');
+// else if (hour >=12 && hour < 18)
+//     console.log('Good Afternoon');
+// else
+//     console.log('Good Evening');
+
+// // Switch Case
+
+let role;
+role = 'guest';
+role = 'moderator';
+
+switch (role){
+    case 'guest':
+        console.log('Guest User');
+        break;
+    case 'moderator':
+        console.log('Moderator User');
+        break;
+    default:
+        console.log('Unknown User');
+        break;
+}
